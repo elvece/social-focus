cc6897176cfa04cc33fe055ae6f933b29365e772
elvece <lvcifferello@gmail.com>
2015-12-14 15:49:17 -0700

    adding notes and reworked structure

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -1,6 +1,15 @@
-// add scripts
 
-$(document).on('ready', function() {
-  console.log('sanity check!');
-});
+var data = [];
 
+//constant stream
+nio.source.socketio(
+'http://brand.nioinstances.com',
+['count_by_network']
+).pipe(nio.pass(function(obj){
+  if (data.length <= 3){
+    data.push(obj);
+  } else {
+    data.splice(0,1);
+    data.push(obj);
+  }
+})).pipe(nio.log(data));
