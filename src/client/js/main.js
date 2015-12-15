96151682f893b83c525076d22247045f6eb64085
elvece <lvcifferello@gmail.com>
2015-12-15 12:32:38 -0700

    pseudo coding compare function

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -1,11 +1,25 @@
 
-var data = [
-{ type: "twitter", count_per_sec: 37.5 },
-{ type: "instagram", count_per_sec: 8.6 },
-{ type: "twitter", count_per_sec: 50.5 },
-{ type: "instagram", count_per_sec: 15.6 }];
+var twitterData = [
+  { type: "twitter", count_per_sec: 46 },
+  { type: "twitter", count_per_sec: 41.6 },
+  { type: "twitter", count_per_sec: 45 },
+  { type: "twitter", count_per_sec: 44 }
+];
+
+var instaData = [
+  { type: "instagram", count_per_sec: 11.58 },
+  { type: "instagram", count_per_sec: 11.58 },
+  { type: "instagram", count_per_sec: 12.03 },
+  { type: "instagram", count_per_sec: 13.05 }
+];
+
+//current index in array
+var currentCount;
+//next index in array
+var nextCount;
 
 //constant stream
+//seperate into two arrays
 // nio.source.socketio(
 // 'http://brand.nioinstances.com',
 // ['count_by_network']
@@ -21,5 +35,13 @@ var data = [
 
 var svgContainer = d3.select('#visualization').append('svg');
 
-//create boundaries
-//make bubble appear based on size
+function compareDifference(){
+  var diff = nextCount - currentCount;
+  currentCount = nextCount;
+  //if diff is positive, thus an increase
+  if (diff > 0){
+    //make circle of how large difference is
+  } else { //if diff is negative, thus a decrease
+    //remove circle of how large difference is
+  }
+}
