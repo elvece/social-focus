f3d0509cf983afaf70bc236a1be78dac3096cf6d
elvece <lvcifferello@gmail.com>
2015-12-15 13:18:18 -0700

    display of diff count working

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -1,60 +1,60 @@
 
-var twitterData = [
-  { type: "twitter", count_per_sec: 46 },
-  { type: "twitter", count_per_sec: 41.6 },
-  { type: "twitter", count_per_sec: 45 },
-  { type: "twitter", count_per_sec: 44 }
-];
+var twitterData = [];
 
-var instaData = [
-  { type: "instagram", count_per_sec: 11.58 },
-  { type: "instagram", count_per_sec: 11.58 },
-  { type: "instagram", count_per_sec: 12.03 },
-  { type: "instagram", count_per_sec: 13.05 }
-];
+var instaData = [];
 
 //current index in array
 var currentCount;
 //next index in array
 var nextCount;
 
+var diff;
+
+var count = 0;
+
 //constant stream
 //seperate into two arrays
-// nio.source.socketio(
-// 'http://brand.nioinstances.com',
-// ['count_by_network']
-// ).pipe(nio.pass(function(obj){
-//   if (data.length <= 3){
-//     data.push(obj);
-//   } else {
-//     data.splice(0,1);
-//     data.push(obj);
-//   }
-// })).pipe(nio.log(data));
+nio.source.socketio(
+'http://brand.nioinstances.com',
+['count_by_network']
+).pipe(nio.func(seperateStreams));
 
 function seperateStreams(chunk){
-  if(chunk.type = "twitter"){
+  if(chunk.type === "twitter"){
     twitterData.push(chunk.count_per_sec);
+    // console.log(twitterData);
   } else {
     instaData.push(chunk.count_per_sec);
+    // console.log(instaData);
   }
 }
+
 
 var svgContainer = d3.select('#visualization').append('svg');
 
 function compareDifference(){
-  var diff = nextCount - currentCount;
-  currentCount = nextCount;
+  var comparison = twitterData.splice(0,2);
+  console.log(comparison)
+  nextCount = twitterData[1];
+  currentCount = twitterData[2];
+  diff = nextCount - currentCount;
+  console.log(diff)
+  // currentCount = nextCount;
   return Math.round(diff);
 }
 
-console.log(compareDifference());
+window.setTimeout(compareDifference, 5000)
+window.setTimeout(makeCircles, 7000)
 
 function makeCircles(){
   //if diff is positive, thus an increase
   if (diff > 0){
+    count ++;
+    $('#twitter').html(count);
     //make number of circles that is diff number
   } else { //if diff is negative, thus a decrease
+    count --;
+    $('#twitter').html(count);
     //remove circle of diff number
   }
 }
