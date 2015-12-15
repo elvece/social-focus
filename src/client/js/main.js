05c992746d2b2b100738464405558a122526a691
elvece <lvcifferello@gmail.com>
2015-12-15 15:02:41 -0700

    live update on dom working

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -10,14 +10,21 @@ var nextCount;
 
 var diff;
 
-var count = 0;
+// var change = compareDifference();
 
-//constant stream
-//seperate into two arrays
-nio.source.socketio(
-'http://brand.nioinstances.com',
-['count_by_network']
-).pipe(nio.func(seperateStreams));
+
+function init(){
+  console.log('init')
+  nio.source.socketio(
+  'http://brand.nioinstances.com',
+  ['count_by_network']
+  ).pipe(nio.func(seperateStreams));
+  if (twitterData.length > 2){
+    compareDifference();
+    console.log(diff)
+    $('#twitter').html(diff);
+  }
+}
 
 function seperateStreams(chunk){
   if(chunk.type === "twitter"){
@@ -33,28 +40,31 @@ function seperateStreams(chunk){
 var svgContainer = d3.select('#visualization').append('svg');
 
 function compareDifference(){
-  var comparison = twitterData.splice(0,2);
-  console.log(comparison)
-  nextCount = twitterData[1];
-  currentCount = twitterData[2];
-  diff = nextCount - currentCount;
-  console.log(diff)
-  // currentCount = nextCount;
-  return Math.round(diff);
+
+    var comparison = twitterData.splice(0,2);
+    console.log(comparison)
+    nextCount = comparison[1];
+    currentCount = comparison[0];
+    diff = nextCount - currentCount;
+    console.log(diff)
+    // currentCount = nextCount;
+    return Math.round(diff);
+
 }
 
-window.setTimeout(compareDifference, 5000)
-window.setTimeout(makeCircles, 7000)
 
 function makeCircles(){
   //if diff is positive, thus an increase
   if (diff > 0){
-    count ++;
-    $('#twitter').html(count);
+    $('#twitter').html(diff);
     //make number of circles that is diff number
   } else { //if diff is negative, thus a decrease
-    count --;
-    $('#twitter').html(count);
+    $('#twitter').html(diff);
     //remove circle of diff number
   }
 }
+
+
+
+window.setInterval(init, 1000);
+
