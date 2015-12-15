4ef79718c8ad142dda6f16c69a1ef16c3c791c1e
elvece <lvcifferello@gmail.com>
2015-12-14 21:44:49 -0700

    adding notes and svg

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -1,15 +1,25 @@
 
-var data = [];
+var data = [
+{ type: "twitter", count_per_sec: 37.5 },
+{ type: "instagram", count_per_sec: 8.6 },
+{ type: "twitter", count_per_sec: 50.5 },
+{ type: "instagram", count_per_sec: 15.6 }];
 
 //constant stream
-nio.source.socketio(
-'http://brand.nioinstances.com',
-['count_by_network']
-).pipe(nio.pass(function(obj){
-  if (data.length <= 3){
-    data.push(obj);
-  } else {
-    data.splice(0,1);
-    data.push(obj);
-  }
-})).pipe(nio.log(data));
+// nio.source.socketio(
+// 'http://brand.nioinstances.com',
+// ['count_by_network']
+// ).pipe(nio.pass(function(obj){
+//   if (data.length <= 3){
+//     data.push(obj);
+//   } else {
+//     data.splice(0,1);
+//     data.push(obj);
+//   }
+// })).pipe(nio.log(data));
+
+
+var svgContainer = d3.select('#visualization').append('svg');
+
+//create boundaries
+//make bubble appear based on size
