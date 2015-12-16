98061c1b1146fce74d35fc0bf0f20035bd295085
elvece <lvcifferello@gmail.com>
2015-12-16 09:21:58 -0700

    took out old styling, updated to account for both streams in index, took out old comments

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -1,22 +1,16 @@
 
+// *** GLOBALS *** //
 var twitterData = [];
-
 var instaData = [];
-
-//current index in array
 var currentCount;
-//next index in array
 var nextCount;
-
 var diff;
+var svg = d3.select('svg');
 
-// var change = getDifference();
-
-
-
+//initaliztion of stream and action functions
+window.setInterval(init, 1000);
 
 function init(){
-  // console.log('init')
   nio.source.socketio(
   'http://brand.nioinstances.com',
   ['count_by_network']
@@ -24,41 +18,29 @@ function init(){
   if (twitterData.length > 2){
     getDifference();
     makeCircleArray();
-    // console.log(diff)
     $('#twitter').html('Difference in Twitter counts: '+diff);
+    $('#insta').html('Difference in Instagram counts: '+diff);
   }
 }
 
+// *** HELPER FUNCTIONS ** //
 function seperateStreams(chunk){
   if(chunk.type === "twitter"){
     twitterData.push(chunk.count_per_sec);
-    // console.log(twitterData);
   } else {
     instaData.push(chunk.count_per_sec);
-    // console.log(instaData);
   }
 }
 
-
-// var svgContainer = d3.select('#visualization').append('svg');
-var svg = d3.select('svg');
-
-function getDifference(){
-
-  var comparison = twitterData.splice(0,2);
-  // console.log(comparison)
-
+function getDifference(data){
+  var comparison = data.splice(0,2);
   nextCount = comparison[1];
   currentCount = comparison[0];
   diff = nextCount - currentCount;
   diff = Math.round(diff);
-  // console.log(diff)
-  // currentCount = nextCount;
-  // return Math.round(diff);
-
 }
 
-
+//with sample values to start
 var circleArray = [10, 20, 30];
 
 function makeCircleArray(){
@@ -85,7 +67,6 @@ function makeCircleArray(){
 }
 
 function updateCircles(data){
-
   //compute data join, returns the update selection
   var circle = svg.selectAll('circle')
     .data(data);
@@ -105,7 +86,7 @@ function updateCircles(data){
     })
     .duration(1000)
     .attr('cy',-10)
-    .remove()
+    .remove();
 
   //set attributes
   circle
@@ -119,5 +100,4 @@ function updateCircles(data){
 }
 
 
-window.setInterval(init, 1000);
 
