e05d41797282adbe2b30c8554da2720f5c106a83
elvece <lvcifferello@gmail.com>
2015-12-15 18:12:49 -0700

    working with adding circles on stream

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -10,18 +10,21 @@ var nextCount;
 
 var diff;
 
-// var change = compareDifference();
+// var change = getDifference();
+
+
 
 
 function init(){
-  console.log('init')
+  // console.log('init')
   nio.source.socketio(
   'http://brand.nioinstances.com',
   ['count_by_network']
   ).pipe(nio.func(seperateStreams));
   if (twitterData.length > 2){
-    compareDifference();
-    console.log(diff)
+    getDifference();
+    makeCircleArray();
+    // console.log(diff)
     $('#twitter').html(diff);
   }
 }
@@ -37,33 +40,70 @@ function seperateStreams(chunk){
 }
 
 
-var svgContainer = d3.select('#visualization').append('svg');
+// var svgContainer = d3.select('#visualization').append('svg');
+var svg = d3.select('svg');
 
-function compareDifference(){
+function getDifference(){
 
-    var comparison = twitterData.splice(0,2);
-    console.log(comparison)
-    nextCount = comparison[1];
-    currentCount = comparison[0];
-    diff = nextCount - currentCount;
-    console.log(diff)
-    // currentCount = nextCount;
-    return Math.round(diff);
+  var comparison = twitterData.splice(0,2);
+  // console.log(comparison)
+
+  nextCount = comparison[1];
+  currentCount = comparison[0];
+  diff = nextCount - currentCount;
+  diff = Math.round(diff);
+  // console.log(diff)
+  // currentCount = nextCount;
+  // return Math.round(diff);
 
 }
 
 
-function makeCircles(){
+var circleArray = [];
+
+function makeCircleArray(){
   //if diff is positive, thus an increase
-  if (diff > 0){
-    $('#twitter').html(diff);
-    //make number of circles that is diff number
-  } else { //if diff is negative, thus a decrease
-    $('#twitter').html(diff);
-    //remove circle of diff number
+  if (diff >= 1) {
+    console.log('greater or equal to 1: '+diff)
+    //adds a number for circle to array
+    for (var i = 0; i < diff; i++) {
+      var num = Math.floor(Math.random() * 10);
+      circleArray.push(num);
+      console.log('positive: '+circleArray)
+    }
   }
+  //if diff is negative, thus a decrease
+  if (diff < 0){
+    //whatever diff is, splice off that many items from array
+    console.log('less than 0: '+diff)
+    circleArray.splice(0, diff);
+    console.log('negative: '+circleArray)
+  }
+  updateCircles(circleArray);
 }
 
+function updateCircles(data){
+
+  //compute data join, returns the update selection
+  var circle = svg.selectAll("circle")
+    .data(data);
+
+  //add incoming circles
+  circle.enter().append("circle")
+    .attr("r", function(d) { return Math.sqrt(d); });
+
+  //enter and update
+  // circle.circle(function(d){return d;});
+
+  //remove old
+  circle.exit().remove()
+
+  //set attributes
+  circle
+    .attr("cy", 60)
+    .attr("cx", function(d, i) { return i * 10 + 30; });
+
+}
 
 
 window.setInterval(init, 1000);
