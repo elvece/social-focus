a81d5877ca77a8edebd59c8d056091911491793e
elvece <lvcifferello@gmail.com>
2015-12-16 15:03:42 -0700

    seperated d3 components

---
+++ src/client/js/d3.js
@@ -0,0 +1,51 @@
+window.constructCircles = (function () {
+
+  function makeCircleArray(data, diff, svg, color){
+    if (diff >= 1) {
+      for (var i = 0; i < diff; i++) {
+        //can be any number, just need a value to generate circle
+        data.push(10);
+      }
+    }
+    if (diff < 0){
+      var val = Math.abs(diff);
+      data.splice(0, val);
+    }
+    updateCircles(data, svg, color);
+  }
+
+  function updateCircles(data, svg, color){
+    //compute data join, returns the update selection
+    var circle = svg.selectAll('circle')
+      .data(data);
+
+    //add incoming circles
+    circle.enter().append('circle')
+      .attr('cy', -10)
+      .attr('r', 10);
+
+    //remove old
+    circle
+      .exit()
+      .transition()
+      .delay(function(d, i) {
+        return i * 100;
+      })
+      .duration(1000)
+      .attr('cy',-10)
+      .remove();
+
+    //set attributes
+    circle
+      .transition()
+      .delay(function(d, i) {
+        return i * 100;
+      })
+      .duration(1000)
+      .attr('fill', color)
+      .attr('cy', 100)
+      .attr('cx', function(d, i) { return i * 25 + 30; });
+  }
+
+  return makeCircleArray;
+})();
--- src/client/js/main.js
+++ src/client/js/main.js
@@ -1,20 +1,23 @@
 window.init = function(){
 
   var services = {},
-      colors = { 'instagram': '#10347A', 'twitter': '#E7AC14', 'negative': 'red'};
+      colors = { 'instagram': '#10347A', 'twitter': '#E7AC14', 'negative': 'darkred'};
 
   function start(){
     nio.source.socketio(
     'http://brand.nioinstances.com',
     ['count_by_network']
     ).pipe(nio.func(seperateStreams));
-    // if (twitterData.length >= 2 && instaData.length >= 2){}
       setStreams();
   }
 
   function seperateStreams(chunk){
     if (!services[chunk.type]){
-      services[chunk.type] = {data: [chunk.count_per_sec], color: colors[chunk.type] , diff: 0, circles: [] };
+      services[chunk.type] = {
+        data: [chunk.count_per_sec],
+        color: colors[chunk.type] ,
+        diff: 0,
+        circles: [] };
     } else {
       services[chunk.type].data.push(chunk.count_per_sec);
     }
@@ -25,9 +28,8 @@ window.init = function(){
       var service = services[key];
       service.diff = getDifference(service.data, service.diff);
       var svg = d3.select('#'+key+'-bowl');
-      makeCircleArray(service.circles, service.diff, svg, service.color);
+      window.constructCircles(service.circles, service.diff, svg, service.color);
       $('#'+key).html('Difference in '+key+ ' counts: '+service.diff);
-      // console.log(service);
     });
   }
 
@@ -39,55 +41,6 @@ window.init = function(){
     diff = Math.round(diff);
     return diff;
   }
-
-  function makeCircleArray(data, diff, svg, color){
-    if (diff >= 1) {
-      for (var i = 0; i < diff; i++) {
-        //needs to not produce 0
-        var num = Math.floor(Math.random() * 10);
-        data.push(num);
-      }
-    }
-    if (diff < 0){
-      var val = Math.abs(diff);
-      data.splice(0, val);
-    }
-    updateCircles(data, svg, color);
-  }
-
-  function updateCircles(data, svg, color){
-    //compute data join, returns the update selection
-    var circle = svg.selectAll('circle')
-      .data(data);
-
-    //add incoming circles
-    circle.enter().append('circle')
-      .attr('cy', -10)
-      .attr('r', 10);
-
-    //remove old
-    circle
-      .exit()
-      .transition()
-      .delay(function(d, i) {
-        return i * 100;
-      })
-      .duration(1000)
-      .attr('cy',-10)
-      .remove();
-
-    //set attributes
-    circle
-      .transition()
-      .delay(function(d, i) {
-        return i * 100;
-      })
-      .duration(1000)
-      .attr('fill', color)
-      .attr('cy', 100)
-      .attr('cx', function(d, i) { return i * 25 + 30; });
-  }
-
 
   setInterval(start, 1000);
 };
